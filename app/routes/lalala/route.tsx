d27783316f61c3286bc485dd56f06a7aa52d2816
Pilar Buchen <76191153+pilarbuchen@users.noreply.github.com>
2024-11-25 12:04:46 -0600

    first commit

---
+++ app/routes/lalala/route.tsx
@@ -0,0 +1,5 @@
+
+import React from 'react';
+export default function Lalala() {
+return <div>lalala</div>;
+}
---
+++ app/routes/ofkpodkf/route.tsx
@@ -0,0 +1,5 @@
+
+import React from 'react';
+export default function Ofkpodkf() {
+return <div>ofkpodkf</div>;
+}
